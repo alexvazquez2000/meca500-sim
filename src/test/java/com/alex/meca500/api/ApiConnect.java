cb78f1b594e46d10cac0e2eba36519055ba65032
Alex Vazquez <alexvazquez2000@yahoo.com>
2026-08-04 14:44:39 -0600

    drive the robot

--- src/test/java/com/alex/meca500/api/ApiConnect.java
+++ src/test/java/com/alex/meca500/api/ApiConnect.java
@@ -21,33 +21,49 @@ public class ApiConnect {
 		parameters.put("status", "active search"); // Contains a space that needs encoding
 		//
 		
-		postCall(baseUrl + "/connect", "{}");
-		postCall(baseUrl + "/activate-and-home", "{}");
+		postCall(baseUrl + "/connect", "{}", 10);
+		postCall(baseUrl + "/activate-and-home", "{}", 10);
 		
-		getCall(baseUrl + "/status", new HashMap<>());
+//		getCall(baseUrl + "/status", new HashMap<>());
+
+		postCall(baseUrl + "/move-joints", """
+				{"j1":0,"j2":0,"j3":0,"j4":0,"j5":0,"j6":0}""", 2000);
+
+		postCall(baseUrl + "/move-joints", """
+				{"j1":0,"j2":0,"j3":0,"j4":0,"j5":0,"j6":-130}""", 2000);
+
 		getCall(baseUrl + "/pose", new HashMap<>());
 
+		//rise to safe Z before any lateral movement
 		postCall(baseUrl + "/move-lin", """
-				{"x":120.5189469808946,"y":74.0243366261341,"z":-42.23348365624428,"alpha":-22.718287925248822,"beta":-28.49299635270986,"gamma":-170.54176424182427}
-				""");
+				{"x": 122.79445, "y": 8.32521, "z": 227.89921, "alpha": -180.30399, "beta": 0.9355, "gamma": 3.04982}""", 1000);
+
+//		//Start to descend
+//		postCall(baseUrl + "/move-lin", """
+//				{"x": 120.79445, "y": -8.32521, "z": 227.89921, "alpha": -180.30399, "beta": 0.9355, "gamma": 3.04982}""", 1000);
 //
+//		postCall(baseUrl + "/move-lin", """
+//				{"x": 119.83676, "y": 3.15258, "z": 218.63574, "alpha": 180.0, "beta": 0.0, "gamma": 2.0}""", 1000);
+//
+		
+		//119.83677,3.15258,218.63574,-180.0,0.0,2.0
 //		postCall(baseUrl + "/move-joints", """
 //				{"j1":10,"j2":10,"j3":10,"j4":10,"j5":10,"j6":10}
-//				""");
+//				""", 1000);
 //		getCall(baseUrl + "/joints", new HashMap<>());
 //		getCall(baseUrl + "/status", new HashMap<>());
 //		
-		postCall(baseUrl + "/move-joints", """
-				{"j1":20,"j2":20,"j3":20,"j4":20,"j5":20,"j6":20}
-				""");
+//		postCall(baseUrl + "/move-joints", """
+//				{"j1":20,"j2":20,"j3":20,"j4":20,"j5":20,"j6":20}
+//				""");
 		
 		getCall(baseUrl + "/joints", new HashMap<>());
 		
-		postCall(baseUrl + "/disconnect", "{}");
+		postCall(baseUrl + "/disconnect", "{}",10);
 		
 	}
 
-	private static void postCall(String url, String jsonParameters) {
+	private static void postCall(String url, String jsonParameters, int sleepMillisecs) {
 		// Build the HttpClient and HttpRequest
 		HttpClient client = HttpClient.newHttpClient();
 		HttpRequest request = HttpRequest.newBuilder()
@@ -60,6 +76,8 @@ public class ApiConnect {
 			// Send the request and capture the response
 			HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
+			Thread.sleep(sleepMillisecs);
+			
 			// Handle the response
 			System.out.println("Status Code: " + response.statusCode());
 			System.out.println("Response Body: " + response.body());
